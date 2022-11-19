f66d1de86b07d0540fbc54506502b948df88b6c2
shwetapund <pundshweta2020@gmail.com>
2022-11-19 10:14:34 -0800

    all method added in one file

---
+++ method.js
@@ -0,0 +1,67 @@
+/*
+
+const arr = ["satyam", "dhirendr", "avishkar"]
+
+console.log(arr)
+
+console.log(arr.toString())    //tostring()
+
+
+console.log(arr.join(" "))    //join
+
+console.log(arr)
+
+console.log(arr)
+*/
+
+/*
+const arr = ["hii", "hello", "friends"]
+
+console.log(arr)
+
+
+arr.shift()             //shift()
+arr.shift()
+
+console.log(arr)
+
+
+
+arr.unshift("A")          //unshift
+console.log(arr)
+
+arr.unshift("oh")
+console.log(arr)
+
+const arr = ["hii", "i", "am", "shweta"]
+console.log(arr)
+
+arr[1] = "o my god"
+console.log(arr)
+
+arr[3] = "shiv"
+console.log(arr)
+*/
+
+//splice
+
+const arr1 = ["mouse", "laptop", "keyboard", "usb", "pendrive"];
+
+console.log(arr1);
+
+arr1.splice(2, 2, "monitor", "mouse pad")
+
+console.log(arr1);
+
+//slice
+
+
+const arr = ["A", "B", "C", "D", "E"];
+
+let pieceArray = arr.slice(1, 2)
+console.log(pieceArray)
+
+console.log(arr);
+
+
+
